6ef275a8ee9f998a0a951de9bd57572053173cd6
jitzzz1996 <43922185+jitzzz1996@users.noreply.github.com>
2021-12-30 23:42:58 +0530

    Initial Commit

---
+++ iOS_Your_Business_Card_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS_Your_Business_Card_App
+//
+//  Created by jitesh bablani on 30/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_Your_Business_Card_App/iOS_Your_Business_Card_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Your_Business_Card_AppApp.swift
+//  iOS_Your_Business_Card_App
+//
+//  Created by jitesh bablani on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Your_Business_Card_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
